b42f9eaa01a64d7d7718dbd7c4df35458e3ccc86
web-py-g <61905059+web-py-g@users.noreply.github.com>
2021-01-07 23:10:38 +0300

    Upload new page

---
+++ js/main.js
@@ -0,0 +1,50 @@
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(time,start,target,dur) {
+            time /= dur / 2;
+            if (time < 1) return target / 2 * time * time + start;
+            time--;
+            return -target / 2 * (time * (time - 2) - 1) + start;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+
+            });
+        });
+    };
+    scrollTo();
+}());
+
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if(window.pageYOffset > 50){
+            header.classList.add('header__active')
+        } else{
+            header.classList.remove('header__active')
+        }
+    }
+}());
